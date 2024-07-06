e93c033460f5834809f06cdb3c35d1e19dd678cb
jaylenperez <jaylenestella24@gmail.com>
2024-07-06 16:05:09 -0500

    add /number/<n> route that displays n is a number only if n is an integer

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+Flask web application that displays "Hello HBNB!" at the root route.
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB'"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB'"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """
+    Displays 'C followed by the value of the text variable
+    (replace underscore _ symbols with space)
+    """
+    display_text = text.replace('_', ' ')
+    return 'C {}'.format(display_text)
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text):
+    """
+    Displays 'Python' , followed by the value of the text variable
+    (replace underscore _ symbols with a space)
+    """
+    display_text = text.replace('_', ' ')
+    return 'Python {}'.format(display_text)
+
+
+@app.route('/number/int:n>', strict_slashes=False)
+def number_route(n):
+    return '{} is a number'.format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
