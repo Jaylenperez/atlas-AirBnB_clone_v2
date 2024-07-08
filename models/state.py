f6589a93962337342701875a1701e30a5fd255d4
jaylenperez <jaylenestella24@gmail.com>
2024-07-08 16:39:32 -0500

    reverting?

--- models/state.py
+++ models/state.py
@@ -1,33 +1,28 @@
 #!/usr/bin/python3
-"""State Module for HBNB project."""
+""" State Module for HBNB project """
 import models
-from models.city import City, storage
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from models.city import City
+from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import String
+from sqlalchemy import Column
 from sqlalchemy.orm import relationship
 from os import getenv
 
-storage_type = getenv("HBNB_TYPE_STORAGE")
-
 
 class State(BaseModel, Base):
-    """State class."""
-
+    """ State class """
     __tablename__ = 'states'
-
-    if storage_type == "db":
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state",
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state",
                           cascade="all, delete-orphan")
 
-
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
-            """Getter attribute for cities."""
+            """Getter attribute for cities if storage is not DBStorage."""
             listofcities = []
-
-            all_cities = storage.all(City)
-            for city in all_cities.values():
+            for city in models.storage.all(City).values():
                 if city.state_id == self.id:
                     listofcities.append(city)
             return listofcities
