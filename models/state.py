3c1a932edcaaddb9de9a667d8145e469e13c9e47
jaylenperez <jaylenestella24@gmail.com>
2024-07-08 16:15:58 -0500

    modify state for getter task 7

--- models/state.py
+++ models/state.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python3
 """State Module for HBNB project."""
 import models
-from models.city import City
+from models.city import City, storage
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
 class State(BaseModel, Base):
     """State class."""
 
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state",
+
+    if storage_type == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state",
                           cascade="all, delete-orphan")
 
-    if getenv('HBNB_TYPE_STORAGE') != 'db':
+
         @property
         def cities(self):
             """Getter attribute for cities."""
             listofcities = []
-            all_cities = models.storage.all(City)
+
+            all_cities = storage.all(City)
             for city in all_cities.values():
                 if city.state_id == self.id:
                     listofcities.append(city)
